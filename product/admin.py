e866c0e6bd7e4736460aa3e2eea64821834e58d7
jmaldanat <jmaldanat@unal.edu.co>
2025-07-10 13:42:29 -0500

    INSTALLED SUMMERNOTE - add django_summernote to INSTALLED_APPS, and update urlpatterns to include Summernote URLs; enhance ProductPrice admin configuration with Summernote support.

--- product/admin.py
+++ product/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from .models import Product, Store, ProductPrice, ProductPriceStatus, PriceHistory
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(ProductPrice)
+class ProductPriceAdmin(SummernoteModelAdmin):
+    """
+    Admin configuration for the ProductPrice model using SummernoteModelAdmin.
+
+    - Utiliza el decorador @admin.register(ProductPrice) para registrar la clase ProductPriceAdmin directamente en el admin de Django, evitando la necesidad de llamar a admin.site.register manualmente.
+    - list_display: Especifica los campos que se mostrarán en la lista de objetos ProductPrice en el panel de administración.
+    - search_fields: Permite buscar productos por nombre ('name') o código EAN ('ean') usando la relación con el modelo Product mediante '__'.
+    - list_filter: Añade filtros laterales para los campos 'store', 'created_on', 'updated_on' y 'status', facilitando la navegación y gestión de los registros en el admin.
+    """
+    list_display = ('product', 'price', 'store', 'created_on', 'updated_on', 'status')
+    # El uso de '__' permite buscar en campos de modelos relacionados (en este caso, 'name' y 'ean' del modelo Product)
+    search_fields = ('product__name', 'product__ean')
+    list_filter = ('store', 'created_on', 'updated_on', 'status')
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Store)
-admin.site.register(ProductPrice)
 admin.site.register(ProductPriceStatus)
 admin.site.register(PriceHistory)
