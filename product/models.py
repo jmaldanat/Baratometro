2e67b56ca71da1ba081cdb95879436deee2ca094
jmaldanat <jmaldanat@unal.edu.co>
2025-07-10 06:38:56 -0500

    CREATE PRODUCT MODEL - Add Product model with migration and update settings for development

--- product/models.py
+++ product/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    ean = models.CharField(max_length=50, unique=True)  # Código EAN/barcode
+    category = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    image_url = models.URLField(blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            # Genera slug automático a partir del nombre
+            self.slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
